c93acf8cf27cd6bef4dd35150e12ffd10711fb02
zakir-jem <zakirjemal889@gmail.com>
2024-11-09 08:38:13 +0300

    banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import Banner from '../../components/Banner/Banner'
 
 const Home = () => {
   return (
     <>
         <Header />
+        <Banner />
         <Footer />
     </>
   )
--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,7 +1,27 @@
+import reactRefresh from "@vitejs/plugin-react";
+import envCompatible from "vite-plugin-env-compatible";
+import svgrPlugin from "vite-plugin-svgr";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // This changes the output dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: "build",
+  },
+
+  envPrefix: "REACT_APP_",
+
+  plugins: [
+    reactRefresh(),
+    envCompatible(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ... svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+});
